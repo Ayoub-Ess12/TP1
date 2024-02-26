3bf1a410768e5cad02469a9df88a24dc28a0ed16
Ayoub-Ess12 <158854903+Ayoub-Ess12@users.noreply.github.com>
2024-02-27 00:30:29 +0100

    first commit

---
+++ exercice 1/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+
+    float note, max = 0, min = 21, i = 0, c = 0;
+
+    do {
+        printf("veuillez entre la note :");
+        scanf("%f", &note);
+        if (note >= 0 && note <= 20) {
+            if (note > max) {
+                max = note;
+                i = 1;
+            } else if (note == max) {
+                i++;
+            }
+            if (note < min) {
+                min = note;
+                c = 1;
+            } else if (note == min) {
+                c++;
+            }
+        } else {
+            printf("note invalide !!");
+        }
+    } while (note != -1);
+    printf("max = %.2f attribuee %.2f fois ", max, i);
+    printf("min = %.2f attribuee %.2f fois ", min, c);
+
+    return 0;
+}
